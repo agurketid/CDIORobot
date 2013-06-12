160b63e19677b447be34c2d407619fde62c29fbe
Casper <cascle01@gmail.com>
2013-06-12 13:44:04 +0200

    Flere Bluetoot tests
    Jeg har lavet nogle flere tests og fået det til at virke. Nu skal der
    bare laves kode

--- src/main/BluetoothTest.java
+++ src/main/BluetoothTest.java
@@ -1,11 +1,16 @@
 package main;
 
+import java.io.OutputStream;
+
 import lejos.nxt.remote.NXTCommand;
 import lejos.nxt.remote.RemoteMotor;
 import lejos.pc.comm.NXTComm;
 import lejos.pc.comm.NXTCommException;
 import lejos.pc.comm.NXTCommFactory;
+import lejos.pc.comm.NXTCommOutputStream;
 import lejos.pc.comm.NXTInfo;
+import lejos.pc.tools.*;
+import lejos.nxt.Motor;
 
 /*
  * A class to test bluetooth - dunno if it works correctly
@@ -15,11 +20,16 @@ public class BluetoothTest {
 
 	public static void main(String[] args) {
 		// BLUETOOTH
+		Motor.A.setPower(100);
+		Motor.A.forward();
 		try {
+			OutputStream stream;
 			NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
-			NXTInfo nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "MaxPower", "00:16:53:0a:6e:9d");
+			NXTInfo nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "MaxPower", "0016530a6e9d");
 			boolean connectionEst = false;
 			do{
+				//NXTCommOutputStream a = (NXTCommOutputStream) nxtComm.getOutputStream();
+				//stream = nxtComm.getOutputStream();
 				connectionEst = nxtComm.open(nxtInfo);
 			} while (!connectionEst);
 			NXTCommand command = new NXTCommand(nxtComm);
@@ -27,6 +37,7 @@ public class BluetoothTest {
 			engine.setSpeed(100);
 			engine.backward();
 		} catch (NXTCommException e) {
+			System.out.println("Fanget\n");
 			e.printStackTrace();
 		}
 	}
